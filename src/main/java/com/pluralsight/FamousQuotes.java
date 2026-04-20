400d37a6362b0fb5541e653d84ca2a0c241e416a
MikaylaBrabham <mikaylabrabham01@gmail.com>
2026-04-20 13:24:29 -0500

    Create quotes and allow user to pick a quote from array

---
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+
+// import scanner
+import java.util.Scanner;
+
+public class FamousQuotes {
+
+    public static void main(String[] args) {
+        // add scanner
+        Scanner scanner = new Scanner(System.in);
+        String [] favQuotes = {
+                "I am the stone that the builder refused, I am the visionary that made the lady sing her blues.",
+                "Where your values lie, that's where your boundaries will be.",
+                "Choose growth over complacency; no matter how fearful you feel.",
+                "Listen to your intuition always, you'll rarely be lead astray",
+                "Be yourself, there wont be anyone else like you again!",
+                "Don't rob the world of your light, don't self sabotage",
+                "I rather bite off more than I can chew and learn to chew it than choose to stay the same",
+                "When in doubt, consider Hannah's prayer",
+                "Remember always that you have free will, but aren't free from the consequences",
+                "Be the change you want to see"
+        };
+        System.out.println("Select a number between 1 and 10: ");
+        int index = scanner.nextInt();
+
+        index--;
+
+        System.out.println(favQuotes[index]);
+
+    }
+}
