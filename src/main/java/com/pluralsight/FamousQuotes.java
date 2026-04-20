a6917fc143a1cd80f5e5738c969ecee96545d647
MikaylaBrabham <mikaylabrabham01@gmail.com>
2026-04-20 13:29:36 -0500

    added try

--- src/main/java/com/pluralsight/FamousQuotes.java
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -9,27 +9,28 @@ public class FamousQuotes {
         // add scanner
         Scanner scanner = new Scanner(System.in);
 
-        // insert quites 1-10 to be chosen in array
-        String [] favQuotes = {
-                "I am the stone that the builder refused, I am the visionary that made the lady sing her blues.",
-                "Where your values lie, that's where your boundaries will be.",
-                "Choose growth over complacency; no matter how fearful you feel.",
-                "Listen to your intuition always, you'll rarely be lead astray",
-                "Be yourself, there wont be anyone else like you again!",
-                "Don't rob the world of your light, don't self sabotage",
-                "I rather bite off more than I can chew and learn to chew it than choose to stay the same",
-                "When in doubt, consider Hannah's prayer",
-                "Remember always that you have free will, but aren't free from the consequences",
-                "Be the change you want to see"
-        };
-        // allow user choose a selection and get a response
-        System.out.println("Select a number between 1 and 10: ");
-        int index = scanner.nextInt();
+      try {
+          // insert quites 1-10 to be chosen in array
+          String[] favQuotes = {
+                  "I am the stone that the builder refused, I am the visionary that made the lady sing her blues.",
+                  "Where your values lie, that's where your boundaries will be.",
+                  "Choose growth over complacency; no matter how fearful you feel.",
+                  "Listen to your intuition always, you'll rarely be lead astray",
+                  "Be yourself, there wont be anyone else like you again!",
+                  "Don't rob the world of your light, don't self sabotage",
+                  "I rather bite off more than I can chew and learn to chew it than choose to stay the same",
+                  "When in doubt, consider Hannah's prayer",
+                  "Remember always that you have free will, but aren't free from the consequences",
+                  "Be the change you want to see"
+          };
+          // allow user choose a selection and get a response
+          System.out.println("Select a number between 1 and 10: ");
+          int index = scanner.nextInt();
 
-        index--;
+          index--;
 
-        System.out.println(favQuotes[index]);
-
+          System.out.println(favQuotes[index]);
+      }
 
     }
 }
